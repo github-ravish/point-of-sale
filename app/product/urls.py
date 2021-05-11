e4e21c73f5a64c083310063733dc8984f4e8e714
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-11 21:40:50 +0530

    product list view final commit

--- app/product/urls.py
+++ app/product/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
+from .views import (
+    ProductListView
+)
+
 app_name = 'product'
 
 urlpatterns = [
+    path('list/<slug:slug>/', ProductListView.as_view(), name='list'),
 ]
--- app/product/views.py
+++ app/product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    View,
+)
 
-# Create your views here.
+from product.permission import RoleRequiredMixin
+from product.models.product import Product
+
+
+class ProductListView(RoleRequiredMixin, ListView):
+    template_name = 'product/list.html'
+
+    def get_queryset(self):
+        shop_slug = self.kwargs.get('slug')
+        return Product.objects.filter(
+            shop__slug=shop_slug
+        )
