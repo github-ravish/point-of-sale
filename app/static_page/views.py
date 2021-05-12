e3a5fda99feb25f8c5cfe349951abec6fe2cc720
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-12 08:30:30 +0530

    user with shop gets redirected to shop list page

--- app/static_page/views.py
+++ app/static_page/views.py
@@ -3,7 +3,7 @@ from django.views.generic import (
     TemplateView,
     View
 )
-from django.contrib.auth.models import Group, Permission
+from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -11,4 +11,6 @@ class HomeView(View):
     template_name = 'static_page/home.html'
 
     def get(self, request, *args, **kwargs):
+        if request.user.user_shop.filter(role=settings.SHOP_ROLE_CHOICE_REVERSE.get('Owner')).exists():
+            return redirect('shop:list')
         return render(request, self.template_name)
