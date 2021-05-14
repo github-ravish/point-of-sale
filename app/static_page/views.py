669247ac4bc492efd1e7f38ec24e5dd1983d0a39
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-15 03:25:04 +0530

    added access for manager

--- app/static_page/views.py
+++ app/static_page/views.py
@@ -9,9 +9,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HomeView(LoginRequiredMixin, View):
     template_name = 'static_page/home.html'
+    role_set1 = [
+        settings.SHOP_ROLE_CHOICE_REVERSE.get('Owner'),
+        settings.SHOP_ROLE_CHOICE_REVERSE.get('Manager'),
+    ]
 
     def get(self, request, *args, **kwargs):
-        if request.user.user_shop.filter(role=settings.SHOP_ROLE_CHOICE_REVERSE.get('Owner')).exists():
+        if request.user.user_shop.filter(role__in=self.role_set1).exists():
             return redirect('shop:list')
         elif request.user.user_shop.filter(role=settings.SHOP_ROLE_CHOICE_REVERSE.get('POS')).exists():
             return redirect('order:home')
