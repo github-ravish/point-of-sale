8a2872872802b6dc13d87624747717f1fc147969
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-15 02:41:35 +0530

    added order home

--- app/order/urls.py
+++ app/order/urls.py
@@ -4,11 +4,15 @@ from .views import (
     OrderCreateView,
     OrderListView
 )
+from shop.views import (
+    ShopListView
+)
 
 
 app_name = 'order'
 
 urlpatterns = [
+    path('home/', ShopListView.as_view(template_name='order/home.html'), name='home'),
     path('create/<slug:shop_slug>/', OrderCreateView.as_view(), name='create'),
     path('list/<slug:shop_slug>/', OrderListView.as_view(), name='list'),
 ]
