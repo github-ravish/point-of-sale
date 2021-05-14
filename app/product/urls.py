fec31f9fae043a319b66f1984f3a5d9ab776a4d2
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-15 02:38:10 +0530

    product home view added

--- app/product/urls.py
+++ app/product/urls.py
@@ -6,10 +6,14 @@ from .views import (
     ProductCreateView,
     ProductListView,
 )
+from shop.views import (
+    ShopListView
+)
 
 app_name = 'product'
 
 urlpatterns = [
+    path('home/', ShopListView.as_view(template_name='product/home.html'), name='home'),
     path('create/<slug:shop_slug>/', ProductCreateView.as_view(), name='create'),
     path('list/<slug:shop_slug>/', ProductListView.as_view(), name='list'),
 ]
