bd23c2fdab508bdc3a399e1534247f727ea48990
Ravish Ranjan <mail.ravishranjan@gmail.com>
2021-05-16 11:54:03 +0530

    quagga init inside a function

--- app/static/scripts/bar_code/quagga.js
+++ app/static/scripts/bar_code/quagga.js
@@ -1,73 +1,78 @@
 quaggaInitialize = false;
-
-Quagga.init({
-	inputStream: {
-		name: "Live",
-		type: "LiveStream",
-		target: document.querySelector('#scanner-container'),
-		constraints: {
-			height: innerWidth,
-			facingMode: "environment"
+function initQuagga() {
+	Quagga.init({
+		inputStream: {
+			name: "Live",
+			type: "LiveStream",
+			target: document.querySelector('#scanner-container'),
+			constraints: {
+				height: innerWidth,
+				facingMode: "environment"
+			},
 		},
-	},
-	decoder: {
-		readers: [
-			"code_128_reader",
-			"ean_reader",
-			"ean_8_reader",
-			"code_39_reader",
-			"code_39_vin_reader",
-			"codabar_reader",
-			"upc_reader",
-			"upc_e_reader",
-			"i2of5_reader"
-		],
-		debug: {
-			showCanvas: true,
-			showPatches: true,
-			showFoundPatches: true,
-			showSkeleton: true,
-			showLabels: true,
-			showPatchLabels: true,
-			showRemainingPatchLabels: true,
-			boxFromPatches: {
-				showTransformed: true,
-				showTransformedBox: true,
-				showBB: true
+		decoder: {
+			readers: [
+				"code_128_reader",
+				"ean_reader",
+				"ean_8_reader",
+				"code_39_reader",
+				"code_39_vin_reader",
+				"codabar_reader",
+				"upc_reader",
+				"upc_e_reader",
+				"i2of5_reader"
+			],
+			debug: {
+				showCanvas: true,
+				showPatches: true,
+				showFoundPatches: true,
+				showSkeleton: true,
+				showLabels: true,
+				showPatchLabels: true,
+				showRemainingPatchLabels: true,
+				boxFromPatches: {
+					showTransformed: true,
+					showTransformedBox: true,
+					showBB: true
+				}
+			}
+		},
+
+	}, function (err) {
+		if (err) {
+			console.log(err);
+			$('#scanner-container').css('display', "none");
+			$('#footer').html("initialized failed");
+			return
+		}
+		quaggaInitialize = true;
+		$('#footer').html("initialized");
+		console.log("Initialization finished. Ready to start");
+	});
+
+	Quagga.onProcessed(function (result) {
+		var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+
+		if (result) {
+			if (result.boxes) {
+				drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+				result.boxes.filter(function (box) {
+					return box !== result.box;
+				}).forEach(function (box) {
+					Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+				});
+			}
+
+			if (result.box) {
+				Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+			}
+
+			if (result.codeResult && result.codeResult.code) {
+				Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
 			}
 		}
-	},
+	});
+}
 
-}, function (err) {
-	if (err) {
-		console.log(err);
-		$('#scanner-container').css('display', "none");
-		return
-	}
-	quaggaInitialize = true;
-	console.log("Initialization finished. Ready to start");
-});
-
-Quagga.onProcessed(function (result) {
-	var drawingCtx = Quagga.canvas.ctx.overlay,
-		drawingCanvas = Quagga.canvas.dom.overlay;
-
-	if (result) {
-		if (result.boxes) {
-			drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-			result.boxes.filter(function (box) {
-				return box !== result.box;
-			}).forEach(function (box) {
-				Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
-			});
-		}
-
-		if (result.box) {
-			Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
-		}
-
-		if (result.codeResult && result.codeResult.code) {
-			Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
-		}
-	}
-});
+$(document).onload = initQuagga();
